99bcd489fdf245de0421695750cfc512489348c2
AbrorjonDev <axmadov.git@gmail.com>
2023-09-27 18:21:04 +0500

    first commit

---
+++ common_prefix.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/longest-common-prefix/
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        common_prefix: str = strs[0]
+        for i in strs[1:]:
+            common_prefix = self.find_common_prefix(common_prefix, i)
+
+        return common_prefix
+
+    def find_common_prefix(self, common_prefix: str, i: str)-> str:
+        for j in range(len(common_prefix)):
+            if j >= len(i) or common_prefix[j]!= i[j]:
+                return common_prefix[:j]
+        return common_prefix
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    assert a.longestCommonPrefix(["flower","flow","flight"]) == "fl"
+    assert a.longestCommonPrefix(["dog","racecar","car"]) == ""
---
+++ valid_parenthesis.py
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/valid-parentheses/
+"""
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    An input string is valid if:
+
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
+        Every close bracket has a corresponding open bracket of the same type.
+
+    Example 1:
+
+    Input: s = "()"
+    Output: true
+
+    Example 2:
+
+    Input: s = "()[]{}"
+    Output: true
+
+    Example 3:
+
+    Input: s = "(]"
+    Output: false
+
+"""
+
+class Solution:
+    characters: dict[str, str] = {'(':')', '{':'}', '[':']'}
+
+    def isValid(self, s: str) -> bool:
+        opened: list[str] = []
+        for i in range(len(s)):
+            if s[i] in self.characters.keys():
+               opened.append(s[i])
+            elif s[i] in self.characters.values():
+                if opened and self.characters[opened[-1]] == s[i]:
+                    del opened[-1]
+                else:
+                    return False
+
+        if opened:
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    a = Solution()
+    assert a.isValid("()[]{}") == True
+    assert a.isValid("([]{})") == True
+    assert a.isValid("({)[]}") == False
+    assert a.isValid("(){[]}") == True
+    assert a.isValid("((){[]}") == False
