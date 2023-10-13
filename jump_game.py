5f0aea18aabcffa08f0a849f5be00ea29ee3756c
AbrorjonDev <axmadov.git@gmail.com>
2023-10-13 07:33:26 +0500

    code style checked by flake8

--- jump_game.py
+++ jump_game.py
@@ -5,21 +5,22 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
         max_reach = 0
-    
+
         for i in range(len(nums)):
             if i > max_reach:
                 return False
-            
+
             max_reach = max(max_reach, i + nums[i])
-            
+
             if max_reach >= len(nums) - 1:
                 return True
-        
+
         return True
 
 
-a = Solution()
+if __name__ == '__main__':
+    a = Solution()
 
-print(a.canJump([2, 3, 0, 0, 0, 0]))
-print(a.canJump([2, 3, 1, 1, 4]))
-print(a.canJump([4,0,4,2,2,0,1,3,3,0,3]))
+    assert a.canJump([2, 3, 0, 0, 0, 0]) is False
+    assert a.canJump([2, 3, 1, 1, 4]) is True
+    assert a.canJump([4, 0, 4, 2, 2, 0, 1, 3, 3, 0, 3]) is True
